3130c4c11fd8ad640d5e920d6fbc7df2089cd1f8
shaokang.sk <shaokang@wepie.com>
2025-11-11 13:01:49 +0800

    添加数据

--- main.go
+++ main.go
@@ -4,18 +4,15 @@ import (
 	"fmt"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
-	}
+	fmt.Println("日志 1: 程序开始执行")
+	fmt.Println("日志 2: 初始化完成")
+	fmt.Println("日志 3: 处理业务逻辑")
+	fmt.Println("日志 4: 数据验证通过")
+	fmt.Println("日志 5: 执行核心操作")
+	fmt.Println("日志 6: 中间处理步骤")
+	fmt.Println("日志 7: 继续处理中")
+	fmt.Println("日志 8: 接近完成")
+	fmt.Println("日志 9: 最后检查")
+	fmt.Println("日志 10: 程序执行完成")
 }
